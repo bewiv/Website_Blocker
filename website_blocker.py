69379670da081d3febe04161cd77f24c40107d0f
bewiv <admarouane@gmail.com>
2018-04-30 12:00:11 +0100

    write in hosts file (same folder) finishing the first part

--- website_blocker.py
+++ website_blocker.py
@@ -4,9 +4,18 @@ from datetime import datetime as dt
 hosts_temp="hosts"
 hosts_path=r"C:\Windows\System32\drivers\etc\hosts" # we add r to pass a row string
 redirect="127.0.0.1"
-website_list=["www.hespress.com","hespress.com"]
+website_list=["www.bewiv.com","bewiv.com"]
 
 while True:
     if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 16):
        print("working time...")
-       time.sleep(5)
+       with open(hosts_temp, 'r+') as file:
+           content=file.read()
+           for website in website_list:
+               if website in content:
+                   pass
+               else:
+                   file.write(redirect+" "+website+"\n")
+    else:
+        print("fun hours...")
+    time.sleep(5)
